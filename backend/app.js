9af83ce082bf365f061053f1ce424727a6f7a997
Geovana Menossi <geovanapissininmenossi@gmail.com>
2024-04-24 13:44:32 -0300

    quarto commit

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const path =  require('path');
+const session = require('express-session');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+app.use(session({
+  secret: 'suaChaveSecreta',
+  resave: false,
+  saveUninitialized: true
+}));
+
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: 'espias'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: 'espias'
 });
 
 db.connect((err) => {
@@ -20,12 +27,22 @@ db.connect((err) => {
   console.log('Conectado ao banco de dados MySQL');
 });
 
+const authenticateUser = (req, res, next) => {
+  if (req.session && req.session.user) {
+    next();
+  } else if (req.path === '/') {
+    next(); 
+  } else {
+    res.redirect('/');
+  }
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Configuração para servir arquivos estáticos
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', authenticateUser, (req, res) => {
   const sql = 'SELECT * FROM usuarios';
   db.query(sql, (err, result) => {
     if (err) {
@@ -36,80 +53,72 @@ app.get('/usuarios', (req, res) => {
   });
 });
 
-app.get('/postagens', (req, res) => {
-    const sql = 'SELECT * FROM postagens';
-    db.query(sql, (err, result) => {
-      if (err) {
-        res.status(500).send('Erro ao buscar usuários');
-      } else {
-        res.send(result);
-      }
-    });
-  });
-
-app.post('/usuarios', (req, res) => {
-  const { nome_usuario, email_usuario, data_nasc_usuario, genero_usuario } = req.body;
-  const sql = 'INSERT INTO usuarios (nome_usuario, email_usuario, data_nasc_usuario, genero_usuario) VALUES (?, ?, ?, ?)';
-  db.query(sql, [nome_usuario, email_usuario, data_nasc_usuario, genero_usuario], (err, result) => {
+app.get('/postagens', authenticateUser, (req, res) => {
+  const sql = 'SELECT * FROM postagens';
+  db.query(sql, (err, result) => {
     if (err) {
+      res.status(500).send('Erro ao buscar postagens');
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.post('/cadastro', (req, res) => {
+  const { txtnome, txtemail, dtnasc, txtgenero, txtsenha } = req.body;
+  const sql = `INSERT INTO usuarios (nome_usuario, email_usuario, data_nasc_usuario, genero_usuario, senha_usuario) VALUES (?, ?, ?, ?, ?)`;
+  db.query(sql, [txtnome, txtemail, dtnasc, txtgenero, txtsenha], (err, result) => {
+    if (err) {
+      console.error('Erro ao criar usuário:', err);
       res.status(500).send('Erro ao criar usuário');
     } else {
-      res.send('Usuário criado com sucesso');
+      console.log('Usuário criado com sucesso');
+      res.redirect('/login?success=cadastroConcluido');
     }
   });
 });
 
-app.put('/usuarios/:id', (req, res) => {
-  const { nome_usuario, email_usuario, data_nasc_usuario, genero_usuario } = req.body;
-  const id = req.params.id;
-  const sql = 'UPDATE usuarios SET nome_usuario=?, email_usuario=?, data_nasc_usuario=?, genero_usuario=? WHERE id_usuarios=?';
-  db.query(sql, [nome_usuario, email_usuario, data_nasc_usuario, genero_usuario, id], (err, result) => {
-    if (err) {
-      res.status(500).send('Erro ao atualizar usuário');
+app.post('/login', (req, res) => {
+  const { txtemail, txtsenha } = req.body;
+  const sql = 'SELECT * FROM usuarios WHERE email_usuario = ? AND senha_usuario = ?';
+  db.query(sql, [txtemail, txtsenha], (err, result) => {
+    if (err || result.length === 0) {
+      res.redirect('/login?error=credenciaisIncorretas');
     } else {
-      res.send('Usuário atualizado com sucesso');
+      req.session.user = result[0];
+      res.redirect('/');
     }
   });
 });
 
-app.delete('/usuarios/:id', (req, res) => {
-  const id = req.params.id;
-  const sql = 'DELETE FROM usuarios WHERE id_usuarios=?';
-  db.query(sql, id, (err, result) => {
-    if (err) {
-      res.status(500).send('Erro ao excluir usuário');
-    } else {
-      res.send('Usuário excluído com sucesso');
-    }
-  });
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
 });
-app.get("/login", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin","*")
-  res.sendFile(path.resolve('../frontend/login.html'));
-});
-app.get("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin","*")
+
+app.get("/", authenticateUser, (req, res) => {
   res.sendFile(path.resolve('../frontend/home.html'));
 });
-app.get("/cadastro", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin","*")
-  res.sendFile(path.resolve('../frontend/cadastro.html'));
-});
-app.get("/home", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin","*")
+
+app.get("/home", authenticateUser, (req, res) => {
   res.sendFile(path.resolve('../frontend/inicio.html'));
 });
-app.get("/notificaoes", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin","*")
+
+app.get("/notificacoes", authenticateUser, (req, res) => {
   res.sendFile(path.resolve('../frontend/notificacoes.html'));
 });
-app.get("/perfil", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin","*")
+
+app.get("/perfil", authenticateUser, (req, res) => {
   res.sendFile(path.resolve('../frontend/perfil.html'));
 });
-app.get("/editar", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin","*")
+
+app.get("/editar", authenticateUser, (req, res) => {
   res.sendFile(path.resolve('../frontend/editar.html'));
+});
+
+app.get("/login", (req, res) => {
+  const { success, error } = req.query;
+  res.sendFile(path.resolve('../frontend/login.html'));
 });
 
 app.listen(port, () => {
